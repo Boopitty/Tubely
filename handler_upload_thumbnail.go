44c1c30eb0b5b2e5afdf04e5e2c2c9c0cff30a80
Emil Auguste <emilrauguste@gmail.com>
2026-06-01 17:50:31 -0400

    Implement thumbnail upload functionality in handlerUploadThumbnail

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
@@ -28,10 +29,61 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
-	// TODO: implement the upload here
+	// Parse the for data
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	// This is the maximum MB size of the form data, including the file. Adjust as needed.
+	maxMemory := int64(10 << 20) // 10 MB
+
+	err = r.ParseMultipartForm(maxMemory)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Couldn't parse form data", err)
+		return
+	}
+
+	// "thumbnail" is the key of the form field that contains the file. Adjust as needed.
+	fileData, fileHeader, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Couldn't read thumbnail file", err)
+		return
+	}
+	defer fileData.Close()
+
+	// The media type of the uploaded file from the header
+	mediaType := fileHeader.Header.Get("Content-Type")
+
+	// The actual image file data as a byte slice
+	imageData, err := io.ReadAll(fileData)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't read thumbnail file data", err)
+		return
+	}
+
+	// Get the video from the db, and check if the user is the correct owner.
+	video, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusNotFound, "Video not found", err)
+		return
+	}
+	if video.UserID != userID {
+		respondWithError(w, http.StatusForbidden, "You are not the owner of this video", nil)
+		return
+	}
+
+	// Update the thumbnail for the video in the database. In this example, we are storing the thumbnail data directly in the database, but in a real application, you would likely want to store it in a file storage service like S3 and save the URL in the database instead.
+	videoThumbnails[video.ID] = thumbnail{
+		data:      imageData,
+		mediaType: mediaType,
+	}
+
+	thumbnailURL := fmt.Sprintf("http://localhost:%s/api/thumbnails/%s", cfg.port, videoID)
+	video.ThumbnailURL = &thumbnailURL
+
+	err = cfg.db.UpdateVideo(video)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't update video thumbnail in database", err)
+		return
+	}
+	respondWithJSON(w, http.StatusOK, video)
 }
